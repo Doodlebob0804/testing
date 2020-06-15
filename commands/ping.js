2b6354f9e76debd5f36d66245c63d376beb31dcc
biggant2 <cwolf1288@gmail.com>
2020-06-15 14:33:08 -0700

    switch to discord.js@12.2.0 and audio

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args) => {
     const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
 }
 
 exports.help = {
