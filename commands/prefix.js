6d575b1f184877fff166b6fc7efbe3bff64ef0e9
biggant2 <cwolf1288@gmail.com>
2020-07-13 18:08:37 -0700

    hotfix

--- commands/prefix.js
+++ commands/prefix.js
@@ -4,6 +4,7 @@ const keyv = new Keyv('sqlite://prefix.db')
 
 exports.run = async (client, message, args) => {
     if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('You need **Manage Server** permissions to be able to use this command.');
+    if(!args[0]) return message.channel.send('Please provide a new prefix.');
     if(args[0].length > 2) return message.channel.send('The prefix cannot be more than 2 characters long.');
     await keyv.set(message.guild.id, args[0]);
     return message.channel.send(`The prefix has been changed to ${args[0]}`)
